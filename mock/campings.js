889ffd6d0f56f4f9046afded60fa8ffb5c4c6c4b
saud maruli <uthrul@gmail.com>
2019-10-23 09:22:41 +0700

    Learn From Keact Ui Kit

---
+++ mock/campings.js
@@ -0,0 +1,34 @@
+const campings = [
+  {
+    id: 1,
+    type: "rv",
+    name: "Stadion Pakansari",
+    description: "Popular spot for runing.",
+    rating: 4.9,
+    distance: 2.9,
+    price: "Free",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipMXnf1a7uPRB2YZqjg7tWsV4FNIdydCUBwAXws=w408-h306-k-no",
+    latlng: {
+      latitude: -6.4944717,
+      longitude: 106.8325989
+    }
+  },
+  {
+    id: 2,
+    type: "tent",
+    name: "TAMAN PEMDA",
+    description: "This is for family time.",
+    rating: 4.9,
+    distance: 2.9,
+    price: "10000",
+    image:
+      "https://lh5.googleusercontent.com/p/AF1QipOohSC8Zg6AKNEd9LVgP58u2w2xkYNau-UjgBIs=w480-h240-k-no",
+    latlng: {
+      latitude: -6.481877,
+      longitude: 106.8255066
+    }
+  }
+];
+
+export { campings };
